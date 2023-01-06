996e7a302bdcc1aec3bbcfe3a40b9a6de62d584e
Anand-Mohanty <110300067+Anandmohanty@users.noreply.github.com>
2023-01-06 18:29:26 +0530

    Add files via upload

---
+++ Program_uc_2.cs
@@ -0,0 +1,28 @@
+﻿namespace EmployeeMangement2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empWage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : "+ empWage);
+                
+        }
+    }
+}
